9b81993424aa342fd17794ff7f75f4fdf737e70e
langroops <langroops@gmail.com>
2022-06-24 22:08:30 -0400

    play video added

--- app/src/components/media-image.component.jsx
+++ app/src/components/media-image.component.jsx
@@ -1,13 +1,21 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { ReactComponent as PlayIcon } from "../icons/play.svg";
 
 const MediaImage = ({ media, campaignName, index }) => {
+  const videoRef = useRef(null);
   const [playVideo, setPlayVideo] = useState(false);
 
   const handleVideoPlay = () => {
     setPlayVideo((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (videoRef.current !== null) {
+      videoRef.current.play();
+    }
+  }, [videoRef, playVideo]);
+
   return (
     <>
       <img
@@ -16,7 +24,7 @@ const MediaImage = ({ media, campaignName, index }) => {
         alt={`${campaignName}_media_${media.media_type}_${index}`}
       ></img>
       {playVideo ? (
-        <video className="media-video">
+        <video className="media-video" ref={videoRef}>
           <source src={media.download_url}></source>
         </video>
       ) : (
